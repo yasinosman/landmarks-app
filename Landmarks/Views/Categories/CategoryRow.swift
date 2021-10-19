df04080047731f7eb566df0af853c4936671201f
yasinosman <yasinosman10@gmail.com>
2021-10-19 21:39:31 +0300

    Add navigation for accessing Featured and List screens

--- Landmarks/Views/Categories/CategoryRow.swift
+++ Landmarks/Views/Categories/CategoryRow.swift
@@ -21,7 +21,10 @@ struct CategoryRow: View {
             ScrollView(.horizontal, showsIndicators: false){
                 HStack(alignment: .top, spacing: 0) {
                     ForEach(items) { landmark in
-                        CategoryItem(landmark: landmark)
+                        NavigationLink(destination: LandmarkDetail(landmark: landmark)){
+                            CategoryItem(landmark: landmark)
+                        }
+                        
                     }
                 }
             }
--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -8,8 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var currentMenu: Tab = Tab.featured
+    
+    enum Tab {
+        case featured
+        case list
+    }
+    
     var body: some View {
-        LandmarkList()
+        TabView {
+            CategoryHome()
+                .tabItem{
+                    Label("Featured", systemImage: "star")
+                }
+                .tag(Tab.featured)
+            
+            LandmarkList()
+                .tabItem{
+                    Label("List", systemImage: "list.bullet")
+                }
+                .tag(Tab.list)
+            
+        }
+        
     }
 }
 
